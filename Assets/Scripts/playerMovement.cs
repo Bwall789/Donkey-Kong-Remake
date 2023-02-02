0f8595e0398db7468a98483bf77a8c91de6b51a0
Bwall789 <53175693+Bwall789@users.noreply.github.com>
2023-02-02 10:57:32 +0000

    player jump

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float MoveSpeed = new float();
+    public float JumpHeight = new float();
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,5 +20,12 @@ public class playerMovement : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement,0,0) * Time.deltaTime * MoveSpeed;
+    
+    if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) <0.001f)
+    {
+         rb.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
+    }
+    
+    
     }
 }
