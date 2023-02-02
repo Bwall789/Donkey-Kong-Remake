1cf432c33670627cc0090f682099a789b619cf42
Bwall789 <53175693+Bwall789@users.noreply.github.com>
2023-02-02 12:23:27 +0000

    more level added

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float MoveSpeed = new float();
+    public float JumpSpeed = new float();
     public float JumpHeight = new float();
 
     private Rigidbody2D rb;
+    private float speed = new float();
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +20,21 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Mathf.Abs(rb.velocity.y) < 0.005f){
+            speed = MoveSpeed;
+        }else{
+            speed = JumpSpeed;
+        }
+
+        Debug.Log(speed);
         var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement,0,0) * Time.deltaTime * MoveSpeed;
+        transform.position += new Vector3(movement,0,0) * Time.deltaTime * speed;
     
-    if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) <0.001f)
-    {
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) <0.001f)
+        {
+        var storeSpeed = MoveSpeed;
          rb.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
-    }
+        }
     
     
     }
