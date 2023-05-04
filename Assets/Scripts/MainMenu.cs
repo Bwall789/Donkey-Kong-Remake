26d5ee9fdec91f6b799c8c4e62d34c0569ed8122
Bwall789 <53175693+Bwall789@users.noreply.github.com>
2023-05-04 10:34:52 +0100

    menu progress

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,27 +6,30 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject manager;
     public GameObject quitOptions;
+    public GameObject optionsMenu;
+    public GameObject awardMenu;
     
     public void StartGame(){
         manager.SetActive(true);
     }
 
     public void OpenAwards(){
-
+        awardMenu.SetActive(true);
     }
 
 
     public void CloseAwards(){
-
+        awardMenu.SetActive(false);
     }
 
     public void OpenOptions(){
+        optionsMenu.SetActive(true);
 
     }
 
 
     public void CloseOptions(){
-
+        optionsMenu.SetActive(false);
     }
 
     public void QuitMenu(){
